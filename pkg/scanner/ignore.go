ce819190cddb254cfa03ebd2907867fb33910b97
AntoninoAdornetto <armaninollc@protonmail.com>
2024-02-04 14:08:23 -0500

    feat: process .gitignore & build regexp patterns The expressions will be used to ignore dirs/files when walking the dir

---
+++ pkg/scanner/ignore.go
@@ -0,0 +1,83 @@
+package scanner
+
+import (
+	"bufio"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type (
+	GitIgnorePattern = []regexp.Regexp
+)
+
+type IgnoreFileOpener interface {
+	Open(fileName string) (*os.File, error)
+}
+
+/*
+Takes a path (`gitIgnorePath`) and a file opener (`fr`) as input and returns a slice of compiled regular expressions -
+that represent the patterns found in the .gitignore file.
+
+The file opener is an interface that allows the caller to provide a custom implementation for opening files.
+
+The function reads the .gitignore file line by line and compiles each line into a regular expression. Empty lines and
+comments (`#`) are ignored.
+*/
+func ProcessIgnorePatterns(gitIgnorePath string, fo IgnoreFileOpener) (GitIgnorePattern, error) {
+	patterns := make(GitIgnorePattern, 0)
+
+	file, err := fo.Open(gitIgnorePath)
+	if err != nil {
+		return patterns, nil
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		pattern := scanner.Text()
+
+		if len(pattern) == 0 || strings.HasPrefix(pattern, "#") {
+			continue
+		}
+
+		patterns = append(patterns, *regexp.MustCompile(formatIgnore(pattern)))
+	}
+
+	if err := scanner.Err(); err != nil {
+		return patterns, err
+	}
+
+	file.Close()
+	return patterns, nil
+}
+
+/*
+Transforms a single .gitignore pattern into a string that can be used to compile a regular expression.
+*/
+func formatIgnore(pattern string) string {
+	res := strings.Builder{}
+
+	if strings.HasPrefix(pattern, "/") {
+		res.WriteString("^\\")
+	}
+
+	for _, v := range pattern {
+		switch v {
+		case '*':
+			res.WriteRune('.')
+			res.WriteRune('*')
+			break
+		case '.':
+			if strings.HasSuffix(res.String(), "*") {
+				res.WriteRune('\\')
+			}
+			res.WriteRune('.')
+			break
+		default:
+			res.WriteRune(v)
+			break
+		}
+	}
+
+	return res.String()
+}
