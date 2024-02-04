a24e682360ae81681263b2ee8e918ea0dcc80150
AntoninoAdornetto <armaninollc@protonmail.com>
2024-02-03 19:56:33 -0500

    chore: sub cmds

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,14 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
 
+	"github.com/AntoninoAdornetto/issue-summoner/cmd/scan"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -36,6 +34,10 @@ func Execute() {
 	}
 }
 
+func subCommands() {
+	rootCmd.AddCommand(scan.ScanCmd)
+}
+
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -46,6 +48,6 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	subCommands()
 }
-
-
