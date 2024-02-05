92fa422a41e4c8886dd53ddd8e8b73d0638fe4ac
AntoninoAdornetto <armaninollc@protonmail.com>
2024-02-04 20:27:25 -0500

    feat: Parse src file & search for tags

---
+++ pkg/parser/tag.go
@@ -0,0 +1,62 @@
+package parser
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+type Tag struct {
+	TagName     string
+	LineNum     uint64
+	FileInfo    os.FileInfo
+	IsProcessed bool
+}
+
+type TagOpener interface {
+	Open(fileName string) (*os.File, error)
+}
+
+type TagParser interface {
+	FindTags(path string, re regexp.Regexp) ([]Tag, error)
+}
+
+func FindTags(path string, re regexp.Regexp, fo TagOpener) ([]Tag, error) {
+	tags := make([]Tag, 0)
+
+	file, err := fo.Open(path)
+	if err != nil {
+		return tags, err
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return tags, err
+	}
+
+	defer file.Close()
+
+	lineNum := uint64(0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if re.Match(scanner.Bytes()) {
+			tags = append(tags, Tag{
+				LineNum:  lineNum + 1,
+				FileInfo: fileInfo,
+			})
+		}
+		lineNum++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return tags, err
+	}
+
+	return tags, nil
+}
+
+func CompileTagRegexp(tagName string) regexp.Regexp {
+	return *regexp.MustCompile(fmt.Sprintf("^(.*)%s(.*)$", tagName))
+}
