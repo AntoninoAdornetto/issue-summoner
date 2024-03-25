a91082253e18568ccd60ab545ea35966f16076fe
AntoninoAdornetto <armaninollc@protonmail.com>
2024-03-25 00:25:59 -0400

    feat: complete device flow and authorize app

--- pkg/scm/github.go
+++ pkg/scm/github.go
@@ -15,12 +15,106 @@ const (
 	BASE_URL      = "https://github.com"
 	CLIENT_ID     = "Iv1.587a6b18c40684ba"
 	GRANT_TYPE    = "urn:ietf:params:oauth:grant-type:device_code"
-	CONFIG_PATH   = "~/.config/issue-summoner/scm.json"
 	ACCESS_TOKEN  = "/access_token"
 	REPO_SCOPE    = "repo"
 	ACCEPT_HEADER = "application/json"
 )
 
+type GitHubManager struct {
+	AccessToken     string
+	RefreshToken    string
+	ExpiresAt       string
+	IsAuthenticated bool
+}
+
+// Authorize satisfies the GitManager interface. Each source code management
+// platform will have their own version of how to authorize a user so that
+// the program can submit issues on the users behalf. This implementation
+// uses GitHubs device oauth flow.
+// First, a user code is created and a browser opens for where we request the code.
+// While the program is waiting for the user to enter the code, we poll an endpoint
+// and check if the user has authorized the app. Once they have done so, an access token
+// is returned from the service and then is written to ~/.config/issue-summoner/config.json
+func (gh *GitHubManager) Authorize() error {
+	var once sync.Once
+	deviceChan := make(chan verifyDeviceResponse)
+	tokenChan := make(chan createTokenResponse)
+	errChan := make(chan error)
+
+	go initDeviceFlow(deviceChan, errChan)
+	device := <-deviceChan
+	go pollTokenService(tokenChan, device, errChan, &once)
+
+	select {
+	case token := <-tokenChan:
+		WriteToken(token.AccessToken, GH)
+		return nil
+	case err := <-errChan:
+		return err
+	}
+}
+
+// @TODO implement IsAuthorized
+// read config file and check for access token presence
+func (gh *GitHubManager) IsAuthorized() bool {
+	return false
+}
+
+func initDeviceFlow(vd chan verifyDeviceResponse, ec chan error) {
+	resp, err := verifyDevice()
+	if err != nil {
+		ec <- err
+		return
+	}
+
+	fmt.Printf("User Code: %s\n", resp.UserCode)
+	err = utils.OpenBrowser(resp.VerificationUri)
+	if err != nil {
+		fmt.Printf(
+			"failed to open default browser. Please visit %s and enter your User Code",
+			resp.VerificationUri,
+		)
+	}
+	vd <- resp
+}
+
+// pollTokenService will make http POST requests to check if the user has successfully
+// authorized the app by entering the user_code into the browser. The function will not
+// poll the endpoint at a higher frequency than the frequency indicated by **interval**
+// in the **verifyDeviceResponse** struct. GitHub will response with a 200 status code and
+// an error response query parameter
+func pollTokenService(
+	tc chan createTokenResponse,
+	device verifyDeviceResponse,
+	ec chan error,
+	once *sync.Once,
+) {
+	// expireTime := time.Now().Add(time.Duration(device.ExpiresIn) * time.Second)
+	expireTime := time.Now().Add(time.Duration(10) * time.Second)
+	ticker := time.NewTicker(time.Duration(device.Interval+1) * time.Second)
+
+	defer ticker.Stop()
+
+	for {
+		if time.Now().After(expireTime) {
+			once.Do(func() {
+				ec <- errors.New("User Code has expired, please re-run the 'issue-summoner report' command to generate a new user code")
+				close(ec)
+			})
+			break
+		}
+
+		select {
+		case <-ticker.C:
+			resp, err := createToken(device.DeviceCode)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				tc <- resp
+			}
+		}
+	}
+}
 
 type createTokenResponse struct {
 	AccessToken string `json:"access_token"`
@@ -53,6 +147,11 @@ func createToken(deviceCode string) (createTokenResponse, error) {
 	tokenErr := handleCreateTokenErr(resp)
 	if tokenErr.Error != "" {
 		return res, errors.New(tokenErr.ErrorDesc)
+	}
+
+	err = json.Unmarshal(resp, &res)
+	if err != nil {
+		return res, err
 	}
 
 	return res, nil
