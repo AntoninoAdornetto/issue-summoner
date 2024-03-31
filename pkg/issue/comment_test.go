20798caac171e679d2948b85a5703ff5b714f160
AntoninoAdornetto <armaninollc@protonmail.com>
2024-03-31 14:58:25 -0400

    test: single line comment parsing

--- pkg/issue/comment_test.go
+++ pkg/issue/comment_test.go
@@ -1,11 +1,57 @@
 package issue_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/AntoninoAdornetto/issue-summoner/pkg/issue"
 	"github.com/stretchr/testify/require"
 )
+
+// TestParseCommentContents_SingleLine is an assertion that we may
+// commonly run into. We have a source code file with a single-line
+// comment that does not expand for many lines. By expanding I mean
+// Example: // @TEST_TODO write unit tests...
+// It's a one line comment with 1 annotation and the comment doesn't
+// go on for many lines.
+func TestParseCommentContents_SingleLine(t *testing.T) {
+	line := "// @TEST_TODO increase test cases for ...."
+	expected := "@TEST_TODO increase test cases for ...."
+
+	stack := issue.CommentStack{Items: make([]string, 0)}
+	builder := strings.Builder{}
+	comment := issue.GetCommentSymbols(".c")
+
+	result, err := comment.ParseCommentContents(line, &builder, stack)
+	require.NoError(t, err)
+	require.Equal(t, expected, result.String())
+}
+
+// TestParseCommentContents_SingleLineExpand asserts that we can
+// build ontop of single-line comments. In other words, we may have
+// single-line coments that expand for many lines.
+// func TestParseCommentContents_SingleLineExpand(t *testing.T) {
+// 	line := "// @TEST_TODO increase code coverage\n\t"
+// 	line2 := "// it's important that we increase code coverage.\n\t"
+// 	line3 := "// it will make our boss happy :)\n\t"
+// 	lines := []string{line, line2, line3}
+//
+// 	expected := `@TEST_TODO increase code coverage
+// 	it's important that we increase code coverage.
+// 	it will make our boss happy :0
+// 	`
+//
+// 	stack := issue.CommentStack{Items: make([]string, 0)}
+// 	builder := strings.Builder{}
+// 	comment := issue.GetCommentSymbols(".c")
+//
+// 	for _, l := range lines {
+// 		_, err := comment.ParseCommentContents(l, &builder, stack)
+// 		require.NoError(t, err)
+// 	}
+//
+// 	require.Equal(t, expected, builder.String())
+// }
 
 func TestCommentSymbols_C(t *testing.T) {
 	m := issue.GetCommentSymbols(".c")
