60121a142c4213873866f8661fa6cac38fbb51ff
AntoninoAdornetto <armaninollc@protonmail.com>
2024-04-03 20:26:31 -0400

    test: skip/proceed w/ line parsing

--- pkg/issue/issue_test.go
+++ pkg/issue/issue_test.go
@@ -24,3 +24,43 @@ func TestGetIssueManager_UnknownIssueType(t *testing.T) {
 	require.Error(t, err)
 	require.Nil(t, issueManager)
 }
+
+func TestSkip_SingleLineTrueGo(t *testing.T) {
+	line := "func main(){ fmt.Printf('Hello World\n')}"
+	require.True(t, issue.Skip(line, issue.GetCommentSymbols(".go")))
+}
+
+func TestSkip_SingleLineFalseGo(t *testing.T) {
+	line := "// single line comment"
+	require.False(t, issue.Skip(line, issue.GetCommentSymbols(".go")))
+}
+
+func TestSkip_SingleLineTruePy(t *testing.T) {
+	line := "def sum(a: int, b: int):"
+	require.True(t, issue.Skip(line, issue.GetCommentSymbols(".py")))
+}
+
+func TestSkip_SingleLineFalsePy(t *testing.T) {
+	line := "# single line comment"
+	require.False(t, issue.Skip(line, issue.GetCommentSymbols(".py")))
+}
+
+func TestSkip_MultiLineStartFalseGo(t *testing.T) {
+	line := "/* begin multi line comment"
+	require.False(t, issue.Skip(line, issue.GetCommentSymbols(".go")))
+}
+
+func TestSkip_MultiLineEndFalseGo(t *testing.T) {
+	line := "End Multi line comment*/"
+	require.False(t, issue.Skip(line, issue.GetCommentSymbols(".go")))
+}
+
+func TestSkip_MultiLineStartFalsePy(t *testing.T) {
+	line := "''' Start Multi line"
+	require.False(t, issue.Skip(line, issue.GetCommentSymbols(".py")))
+}
+
+func TestSkip_MultiLineEndFalsePy(t *testing.T) {
+	line := "End Multi line comment'''"
+	require.False(t, issue.Skip(line, issue.GetCommentSymbols(".py")))
+}
