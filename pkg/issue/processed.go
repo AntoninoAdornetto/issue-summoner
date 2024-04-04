7a745d1bf0bf08bb94e37ddbf7951d76e7d098e6
AntoninoAdornetto <armaninollc@protonmail.com>
2024-04-04 01:59:28 -0400

    chore: satisfy interface will impl soon

--- pkg/issue/processed.go
+++ pkg/issue/processed.go
@@ -1,9 +1,17 @@
 package issue
 
+import (
+	"os"
+)
+
 type ProcessedIssue struct {
 	Issues []Issue
 }
 
-func (pi *ProcessedIssue) Scan() ([]Issue, error) {
-	return nil, nil
+func (pi *ProcessedIssue) Scan(file *os.File) ([]Issue, error) {
+	return pi.Issues, nil
+}
+
+func (pi *ProcessedIssue) ParseComment(arg ParseCommentParams) error {
+	return nil
 }
