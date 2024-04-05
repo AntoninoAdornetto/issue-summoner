5c734b230b915a11b0c19bfeb7e221ffa78f41c5
AntoninoAdornetto <armaninollc@protonmail.com>
2024-04-05 00:19:29 -0400

    fix: end line number bug

--- pkg/issue/pending.go
+++ pkg/issue/pending.go
@@ -2,6 +2,8 @@ package issue
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,112 +15,124 @@ type PendingIssue struct {
 	Issues     []Issue
 }
 
-func (pi *PendingIssue) Scan(file *os.File) ([]Issue, error) {
+func (pi *PendingIssue) GetIssues() []Issue {
+	return pi.Issues
+}
+
+func (pi *PendingIssue) Scan(file *os.File) error {
 	lineNum := uint64(0)
 	scanner := bufio.NewScanner(file)
 	comment := GetCommentSymbols(filepath.Ext(file.Name()))
 
-	// @TODO utilize the file info obj
-	_, err := file.Stat()
+	fileInfo, err := file.Stat()
 	if err != nil {
-		return pi.Issues, err
+		return err
 	}
 
 	for scanner.Scan() {
 		lineNum++
 		currentLine := scanner.Text()
-		lineType := EvalSourceLine(strings.TrimLeftFunc(currentLine, unicode.IsSpace), comment)
+		lineType, symbol := EvalSourceLine(
+			strings.TrimLeftFunc(currentLine, unicode.IsSpace),
+			comment,
+		)
 
 		if lineType == LINE_TYPE_SRC_CODE {
 			continue
 		}
 
 		arg := ParseCommentParams{
-			LineText: currentLine,
-			LineNum:  lineNum,
-			LineType: lineType,
-			Scanner:  scanner,
-			Comment:  comment,
+			LineText:      currentLine,
+			LineType:      lineType,
+			LineNum:       &lineNum,
+			Scanner:       scanner,
+			Comment:       comment,
+			CommentSymbol: symbol,
+			FileInfo:      fileInfo,
 		}
 
 		if err := pi.ParseComment(arg); err != nil {
-			return pi.Issues, err
+			return err
 		}
 	}
 
-	return pi.Issues, nil
+	return nil
 }
 
-// ParseComment will parse lines of text that are read from a buffer and check if a given line
-// contains an issue annotation. If found, details about the issue are parsed from by reading
-// subsequent lines until we reach a line that is not a comment. When a line is not a comment,
-// the base case clause executed and we stop scanning the buffer.
+// @TODO split single and multi line parsing into different functions
+// there is an error atm where we fail to build the entire description for
+// multi line comments. This is because we break out of the iteration when
+// the line we are parsing is not the begining or ending of a multi line commment
 func (pi *PendingIssue) ParseComment(arg ParseCommentParams) error {
+	issue := Issue{StartLineNumber: *arg.LineNum}
 	description := strings.Builder{}
-	issue := Issue{StartLineNumber: arg.LineNum}
 
 	for {
 		annotated := containsAnnotation(arg.LineText, pi.Annotation)
 		if annotated && issue.AnnotationLineNumber == 0 {
-			issue.Title = buildTitle(arg.LineText, pi.Annotation)
-			issue.AnnotationLineNumber = arg.LineNum
-			issue.IsMultiLine = arg.LineType == LINE_TYPE_MULTI
-			issue.IsSingleLine = arg.LineType == LINE_TYPE_SINGLE
+			pi.processAnnotationMetaData(arg.LineText, arg.LineType, arg.LineNum, &issue)
 		} else if issue.AnnotationLineNumber > 0 {
-			nextDescription := buildDescription(arg.LineText, arg.Comment)
-			description.WriteString(nextDescription)
+			descriptionText, err := buildDescription(arg.LineText, arg.LineType, arg.CommentSymbol)
+			if err != nil {
+				return err
+			}
+			description.WriteString(descriptionText)
 		}
 
+		issue.EndLineNumber = *arg.LineNum
 		if !arg.Scanner.Scan() {
-			issue.EndLineNumber = arg.LineNum
-			arg.LineNum = arg.LineNum - issue.StartLineNumber
 			break
 		}
 
-		arg.LineText = arg.Scanner.Text()
-		nextLineType := EvalSourceLine(
-			strings.TrimLeftFunc(arg.LineText, unicode.IsSpace),
+		nextLine := arg.Scanner.Text()
+		arg.LineText = nextLine
+		nextLineType, _ := EvalSourceLine(
+			strings.TrimLeftFunc(nextLine, unicode.IsSpace),
 			arg.Comment,
 		)
 
+		*arg.LineNum++
 		if nextLineType == LINE_TYPE_SRC_CODE {
-			issue.EndLineNumber = arg.LineNum
-			arg.LineNum = arg.LineNum - issue.StartLineNumber
 			break
 		}
-
-		arg.LineNum++
 	}
 
 	if issue.AnnotationLineNumber > 0 {
 		issue.Description = strings.TrimLeftFunc(description.String(), unicode.IsSpace)
+		issue.FileInfo = arg.FileInfo
+		issue.ID = fmt.Sprintf("%s-%d", arg.FileInfo.Name(), issue.AnnotationLineNumber)
 		pi.Issues = append(pi.Issues, issue)
 	}
 
-	return arg.Scanner.Err()
+	return nil
 }
 
-// buildTitle takes a line and issue annotation as input and
-// returns a string that removes leading white space and the
-// annotation.
-// Example:
-// buildTitle("// @ANNOTATION comment", "@ANNOTATION") -> "comment"
-func buildTitle(line string, annotation string) string {
-	remainingText := strings.SplitAfter(line, annotation)[1]
+func (pi *PendingIssue) processAnnotationMetaData(
+	line string,
+	lineType string,
+	lineNum *uint64,
+	issue *Issue,
+) {
+	remainingText := strings.SplitAfter(line, pi.Annotation)[1]
+	issue.Title = strings.TrimLeftFunc(remainingText, unicode.IsSpace)
+	issue.AnnotationLineNumber = *lineNum
+	issue.IsMultiLine = lineType == LINE_TYPE_MULTI
+	issue.IsSingleLine = lineType == LINE_TYPE_SINGLE
+}
+
+func buildDescription(line string, lineType string, symbol string) (string, error) {
+	if lineType == LINE_TYPE_SRC_CODE {
+		return "", errors.New("line type should be single or multi")
+	}
+	return processCommentDescription(line, symbol), nil
+}
+
+// processCommentDescription returns all text after the comment symbol
+func processCommentDescription(line string, symbol string) string {
+	remainingText := strings.SplitAfter(line, symbol)[1]
 	return strings.TrimLeftFunc(remainingText, unicode.IsSpace)
 }
 
-// buildDescription removes leading white space and the single line
-// comment symbols and returns a new string that can be used to append
-// the issue description.
-// @TODO remove hardcode indexing of singleLine comment symbol
-// think of a new solution to build description text without having
-// to hardcode and think of how we are going to handle multi line comments
-func buildDescription(line string, c Comment) string {
-	remainingText := strings.SplitAfter(line, c.SingleLineSymbols[0])[1]
-	return strings.TrimLeftFunc(remainingText, unicode.IsSpace)
-}
-
-func containsAnnotation(line string, tag string) bool {
-	return strings.Contains(line, tag)
+func containsAnnotation(line string, annotation string) bool {
+	return strings.Contains(line, annotation)
 }
