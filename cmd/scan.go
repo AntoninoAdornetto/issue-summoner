db243bf4161955c7d6ec589ebf42bf3d00d091d7
AntoninoAdornetto <armaninollc@protonmail.com>
2024-04-19 18:50:51 -0400

    refactor: report cmd will use similar flags added a common file for flag reuse

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,15 +1,12 @@
 /*
-Copyright © 2024 Antonino Adornetto
+Copyright © 2024 AntoninoAdornetto
 */
 package cmd
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/AntoninoAdornetto/issue-summoner/pkg/issue"
-	"github.com/AntoninoAdornetto/issue-summoner/pkg/scm"
 	"github.com/AntoninoAdornetto/issue-summoner/pkg/ui"
 	"github.com/spf13/cobra"
 )
@@ -18,58 +15,27 @@ var ScanCmd = &cobra.Command{
 	Use:   "scan",
 	Short: "scans source code for Issue annotations (actionable comments)",
 	Long: `scans your git project for comments that include issue annotations.
-		The comment is used for reporting purposes to see what actionable comments your
-		project contains. This can give you an idea of all the issues in your code base
-		prior to uploading them to a source code management platform.
-		`,
+Issue annotations can be as simple as @TODO or any annotation that you see
+fit. The only requirement is that the annotation resides in a single or multi
+line comment. Once found, you can see details about the located comment using
+the verbose flag. The scan command is a preliminary command to the report 
+command. Report will actually publish the located comments to your favorite
+source code management platform. Scan is for reviewing the issue annotations
+that reside in your code base.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		annotation, err := cmd.Flags().GetString("annotation")
+		annotation, ignorePath, path := handleCommonFlags(cmd)
+
+		verbose, err := cmd.Flags().GetBool(flag_verbose)
 		if err != nil {
 			ui.LogFatal(err.Error())
 		}
 
-		ignorePath, err := cmd.Flags().GetString("gitignore")
+		mode, err := cmd.Flags().GetString(flag_mode)
 		if err != nil {
 			ui.LogFatal(err.Error())
 		}
 
-		verbose, err := cmd.Flags().GetBool("verbose")
-		if err != nil {
-			ui.LogFatal(err.Error())
-		}
-
-		path, err := cmd.Flags().GetString("path")
-		if err != nil {
-			ui.LogFatal(err.Error())
-		}
-
-		mode, err := cmd.Flags().GetString("mode")
-		if err != nil {
-			ui.LogFatal(err.Error())
-		}
-
-		if path == "" {
-			wd, err := os.Getwd()
-			if err != nil {
-				ui.LogFatal(err.Error())
-			}
-			path = wd
-		}
-
-		if ignorePath == "" {
-			ignorePath = filepath.Join(path, ".gitignore")
-		}
-
-		gitIgnore, err := os.Open(ignorePath)
-		if err != nil {
-			ui.LogFatal(err.Error())
-		}
-
-		ignorePatterns, err := scm.ParseIgnorePatterns(gitIgnore)
-		if err != nil {
-			ui.LogFatal(err.Error())
-		}
-
+		ignorePatterns := gitIgnorePatterns(ignorePath)
 		im, err := issue.NewIssueManager(mode, annotation)
 		if err != nil {
 			ui.LogFatal(err.Error())
@@ -82,35 +48,25 @@ var ScanCmd = &cobra.Command{
 
 		issues := im.GetIssues()
 		if len(issues) > 0 {
-			fmt.Println(
-				ui.SuccessTextStyle.Render(
-					fmt.Sprintf(
-						"\nFound %d (%s) issue annotations in your project.",
-						len(issues),
-						annotation,
-					),
-				),
-			)
+			success := fmt.Sprintf("Found %d issue annotations using %s", len(issues), annotation)
+			fmt.Println(ui.SuccessTextStyle.Render(success))
 		} else {
-			fmt.Println(ui.SecondaryTextStyle.Render(fmt.Sprintf("\nNo tags were located in your project using the annotation %s", annotation)))
+			fmt.Println(ui.SecondaryTextStyle.Render(fmt.Sprintf("%s %s", no_issues, annotation)))
+			return
 		}
 
 		if verbose {
 			issue.PrintTagResults(issues, ui.DimTextStyle, ui.PrimaryTextStyle)
 		} else {
-			fmt.Println(
-				ui.SecondaryTextStyle.Render(
-					"Tip: run issue-summoner scan -v (verbose) for more details about the tag annotations that were found",
-				),
-			)
+			fmt.Println(ui.SecondaryTextStyle.Render(tip_verbose))
 		}
 	},
 }
 
 func init() {
-	ScanCmd.Flags().StringP("path", "p", "", "path to local git repo")
-	ScanCmd.Flags().StringP("gitignore", "g", "", "gitignore file path")
-	ScanCmd.Flags().StringP("mode", "m", "pending", "i = issued | issues or p = pending")
-	ScanCmd.Flags().BoolP("verbose", "v", false, "log information about each issue found")
-	ScanCmd.Flags().StringP("annotation", "a", "@TODO", "Issue Annotation program will search for")
+	ScanCmd.Flags().StringP(flag_path, shortflag_path, "", flag_desc_path)
+	ScanCmd.Flags().StringP(flag_gignore, shortflag_gignore, "", flag_desc_gignore)
+	ScanCmd.Flags().StringP(flag_mode, shortflag_mode, issue.PENDING_ISSUE, flag_desc_mode)
+	ScanCmd.Flags().BoolP(flag_verbose, shortflag_verbose, false, flag_desc_verbose)
+	ScanCmd.Flags().StringP(flag_annotation, shortflag_annotation, "@TODO", flag_desc_annotation)
 }
