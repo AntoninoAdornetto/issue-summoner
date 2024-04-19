b8e54488d7738ae9417e4a57ab4d3be9d997a389
AntoninoAdornetto <armaninollc@protonmail.com>
2024-04-18 22:04:47 -0400

    docs: update file description

--- pkg/issue/issue.go
+++ pkg/issue/issue.go
@@ -5,17 +5,18 @@ requires attention.
 
 Issues are discovered by parsing each line in a source file. When a file is
 first opened, we determine the syntax used to denote single and multi line comments
-by checking the extenion of the file. I.E main.c, main.go, main.cpp and so on.
-This will allow us to locate single and multi line comment symbols in each source file.
+by checking the extension of the file. I.E main.c, main.go, main.cpp and so on.
+This will allow the program to understand what comment symbols to search for.
 
 As we scan each line, we check if the line contains the prefix notation for a single
 or multi line comment. If it contains a single line comment prefix, we parse and get
-all text after the prefix and annotation, if it exists. If an issue annotation was discovered,
-it qualifies for an issue and we will append a new issue object onto an issues slice.
+all text after the prefix and annotation. If an issue annotation was located,
+it qualifies as an issue and we will be appended as a new issue object onto an issues slice.
 
-In the case of a multi line comment prefix, we will continue to scan subsequent lines until
-reaching the multi line comment suffix notation. At that point we will check if an issue annotation
-was discovered. If so, we append that new issue item onto the issues slice.
+In the case locating of a multi line comment prefix, we will continue to scan subsequent lines until
+reaching the multi line comment suffix. At that point, we will do the same check for the single line
+comment and check that an issue annotation was located and push onto the issues slice if it contained
+the annotation.
 
 There are two types of issues, pending and processed. Pending issues are those that have not yet been
 uploaded to a source code management platform. Processed issues are issues that have been uploaded to a
