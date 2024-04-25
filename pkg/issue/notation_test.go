d89769d14727edb74290592a584f8fa1a7b2ba63
AntoninoAdornetto <armaninollc@protonmail.com>
2024-04-24 23:13:39 -0400

    test: locate prefix indices & determine line type

--- pkg/issue/notation_test.go
+++ pkg/issue/notation_test.go
@@ -58,3 +58,113 @@ func TestNewCommentNotationDefault(t *testing.T) {
 	)
 	require.Equal(t, notation.NewLinePrefixRe, issue.CommentNotations["default"].NewLinePrefixRe)
 }
+
+// should find the start and end indices of the single line prefix bytes and return the comment
+// type as a single line comment when provided with a byte slice that contains a sinle line comment in c
+func TestFindPrefixLocationSingleLineCommentC(t *testing.T) {
+	notation := issue.NewCommentNotation(".c")
+	line := []byte("int n = 5; // single line comment using the c programming language")
+	expectedLocations, expectedCommentType := []int{11, 13}, issue.SINGLE_LINE_COMMENT
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should find the start and end indices of the multi line prefix bytes and return the comment
+// type as a multi line comment when provided with a byte slice that contains a sinle line comment in c
+func TestFindPrefixLocationMultiLineCommentC(t *testing.T) {
+	notation := issue.NewCommentNotation(".c")
+	line := []byte("/* multi line comment using the c programming language")
+	expectedLocations, expectedCommentType := []int{0, 2}, issue.MULTI_LINE_COMMENT
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should return an empty integer slice and an empty string as the comment type when a byte slice
+// is provided as input that does not contain a single or multi line comment
+func TestFindPrefixLocationNoCommentC(t *testing.T) {
+	notation := issue.NewCommentNotation(".c")
+	line := []byte("int main(argc int argv **char) {return 0; }")
+	expectedLocations, expectedCommentType := []int(nil), ""
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should find the start and end indices of the single line prefix bytes and return the comment
+// type as a single line comment when provided with a byte slice that contains a sinle line comment in go
+func TestFindPrefixLocationSingleLineCommentGo(t *testing.T) {
+	notation := issue.NewCommentNotation(".go")
+	line := []byte("// single line comment using the go programming language")
+	expectedLocations, expectedCommentType := []int{0, 2}, issue.SINGLE_LINE_COMMENT
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should find the start and end indices of the multi line prefix bytes and return the comment
+// type as a multi line comment when provided with a byte slice that contains a sinle line comment in go
+func TestFindPrefixLocationMultiLineCommentGo(t *testing.T) {
+	notation := issue.NewCommentNotation(".go")
+	line := []byte("/* multi line comment using the go programming language")
+	expectedLocations, expectedCommentType := []int{0, 2}, issue.MULTI_LINE_COMMENT
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should return an empty integer slice and an empty string as the comment type when a byte slice
+// is provided as input that does not contain a single or multi line comment
+func TestFindPrefixLocationNoCommentGo(t *testing.T) {
+	notation := issue.NewCommentNotation(".go")
+	line := []byte("func main() { fmt.Println('Hello World') }")
+	expectedLocations, expectedCommentType := []int(nil), ""
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should find the start and end indices of the single line prefix bytes and return the comment
+// type as a single line comment when provided with a byte slice that contains a sinle line comment in python
+func TestFindPrefixLocationSingleLineCommentPython(t *testing.T) {
+	notation := issue.NewCommentNotation(".py")
+	line := []byte("# single line comment using the python programming language")
+	expectedLocations, expectedCommentType := []int{0, 1}, issue.SINGLE_LINE_COMMENT
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should find the start and end indices of the multi line prefix bytes and return the comment
+// type as a multi line comment when provided with a byte slice that contains a sinle line comment in c
+func TestFindPrefixLocationMultiLineCommentPython(t *testing.T) {
+	notation := issue.NewCommentNotation(".py")
+	line := []byte("\"\"\" multi line comment using the py programming language \"\"\"")
+	expectedLocations, expectedCommentType := []int{0, 3}, issue.MULTI_LINE_COMMENT
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should return an empty integer slice and an empty string as the comment type when a byte slice
+// is provided as input that does not contain a single or multi line comment
+func TestFindPrefixLocationNoCommentPython(t *testing.T) {
+	notation := issue.NewCommentNotation(".py")
+	line := []byte("n = 5")
+	expectedLocations, expectedCommentType := []int(nil), ""
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
+
+// should find the start and end indices of the single line prefix bytes and return the comment
+// type as a single line comment when provided with a byte slice that contains a sinle line comment in markdown
+func TestFindPrefixLocationSingleLineCommentMarkdown(t *testing.T) {
+	notation := issue.NewCommentNotation(".md")
+	line := []byte("<!-- single line comment using markdown -->")
+	expectedLocations, expectedCommentType := []int{0, 4}, issue.SINGLE_LINE_COMMENT
+	actualLocations, actualCommentType := notation.FindPrefixLocations(line)
+	require.Equal(t, expectedLocations, actualLocations)
+	require.Equal(t, expectedCommentType, actualCommentType)
+}
