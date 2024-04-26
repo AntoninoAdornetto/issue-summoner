1c9061d3e82997176f3b76022e98e74f30878573
AntoninoAdornetto <armaninollc@protonmail.com>
2024-04-26 01:47:19 -0400

    docs(todo): implement processed issue functions

--- pkg/issue/processed.go
+++ pkg/issue/processed.go
@@ -5,6 +5,14 @@ import (
 	"regexp"
 )
 
+/*
+* @TODO implement the processed issues logic
+* The functions implementations should satisfy the IssueManager interface and allow the
+* user of the program to walk the project, and locate comments/issues that have
+* been reported. This will be helpful in later functionality where we can remove
+* comments once issues have been marked as resolved.
+ */
+
 type ProcessedIssue struct {
 	Annotation string
 	Issues     []Issue
