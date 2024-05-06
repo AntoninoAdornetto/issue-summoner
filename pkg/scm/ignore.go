5521aa99d8a55593a49e0143c2463a3811442a02
AntoninoAdornetto <armaninollc@protonmail.com>
2024-05-06 00:23:19 -0400

    docs: update todo

--- pkg/scm/ignore.go
+++ pkg/scm/ignore.go
@@ -20,14 +20,13 @@ type IgnorePattern = regexp.Regexp
 /*
 @TODO add ! (not) operator support for ignoring specific files/directories.
 
-The ParseIgnorePatterns can handle most of the common patterns found in a gitignore
-file. However, there are scenarios where this function will fail to build proper expressions
-and cause the program to crash. There are large open source projects I have tested the program on
-that crash the program and we should resolve this immediately.
-
+The ParseIgnorePatterns func can handle most of the common patterns found in a gitignore file.
+However, there are scenarios where this function will fail to build proper expressions and
+cause the program to crash. I've tested on many open source projects and for some we cannot
+run the program at all. We should resolve this immediately.
 Here is an example of some patterns that are not yet supported:
 
-1. Ignore files in a specific directory, but not its subdirectories:
+1. Ignore files in a specific directory, but not its sub-directories:
 directory_to_ignore/*
 !directory_to_ignore/*
 
@@ -38,7 +37,7 @@ directory_to_ignore/*
 3. Ignore all files in a directory, including hidden files:
 directory_to_ignore/**
 !directory_to_ignore/
-!directory_to_ignore/*
+!directory_to_ignore
 */
 func ParseIgnorePatterns(r io.Reader) ([]IgnorePattern, error) {
 	regexps := make([]IgnorePattern, 0)
