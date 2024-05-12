1e2e7a15c626bf889048f49bb892f77b213ca860
AntoninoAdornetto <armaninollc@protonmail.com>
2024-05-12 05:05:12 -0400

    fix: regexp compilation errs. rewrite or lib it this is a temp fix. there are bad side effects with the current approach

--- pkg/scm/ignore.go
+++ pkg/scm/ignore.go
@@ -1,6 +1,6 @@
 /*
 The functions in this file are responsible for building a slice of regular expressions
-base on patterns in a .gitignore file. The patterns are used to help the program adhere
+based on patterns in a .gitignore file. The patterns are used to help the program adhere
 to the same rules that a .gitignore pattern applies to git repos. The result, we do not
 parse source code that does not need to be parsed. For example, we would never want to
 parse a god forsaken node modules folder!
@@ -17,6 +17,19 @@ import (
 
 type IgnorePattern = regexp.Regexp
 
+/*
+@TODO broken gitignore pattern parsing
+ParseIgnorePatterns function is used to determine if a file should be scanned when walking
+the project dir. I've discovered issues when testing the program on large open source projects,
+such Elasticsearch (java), and Pandas (python). What I found is that some gitignore patterns will
+either cause files to be scanned, when they shouldn't be, or ignore files that should be scanned.
+A decision will need to be made about if a library will be utilized or rewrite the implementation
+to account for all the different edgecases. We may not need to reinvent the wheel here.
+go-ignore is an option but it's quite old, around 10 years old I believe. Check for other libs.
+
+For the meantime, I will use regexp.Compile over MustCompile. If an error is encountered,
+we skip through the remaining lines of the gitignore file.
+*/
 func ParseIgnorePatterns(r io.Reader) ([]IgnorePattern, error) {
 	regexps := make([]IgnorePattern, 0)
 	buf := &bytes.Buffer{}
@@ -33,7 +46,11 @@ func ParseIgnorePatterns(r io.Reader) ([]IgnorePattern, error) {
 		}
 
 		if buf.Len() > 0 {
-			re := regexp.MustCompile(buf.String())
+			re, err := regexp.Compile(buf.String())
+			if err != nil {
+				buf.Reset()
+				continue
+			}
 			regexps = append(regexps, *re)
 			buf.Reset()
 		}
