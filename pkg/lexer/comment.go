3a3acc4a87fdc78be2f6429a09b1d44c1602b925
AntoninoAdornetto <armaninollc@protonmail.com>
2024-05-12 05:10:35 -0400

    refactor: moved for reuse with other lexers

--- pkg/lexer/comment.go
+++ pkg/lexer/comment.go
@@ -16,3 +16,10 @@ func (c *Comment) Prepare(fileName string, index int) {
 func (c *Comment) Validate() bool {
 	return len(c.Source) > 0
 }
+
+func pushComment(comment *Comment, comments *[]Comment, fileName string, index int) {
+	if comment.Validate() {
+		comment.Prepare(fileName, index)
+		*comments = append(*comments, *comment)
+	}
+}
