f4d0bb4116ef25de60dbf8572a719454bce4917b
AntoninoAdornetto <armaninollc@protonmail.com>
2024-05-19 16:23:07 -0400

    refactor: reduce dry path find code & rename vars

--- pkg/scm/git.go
+++ pkg/scm/git.go
@@ -1,13 +1,13 @@
 package scm
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
-	"os/exec"
 	"os/user"
 	"path/filepath"
-	"strings"
 )
 
 const (
@@ -16,37 +16,31 @@ const (
 	BITBUCKET = "bitbucket"
 )
 
-// @TODO can GlobalUserName and RepoName functions be deleted?
-// We are now using the device flow and the mentioned functions could be useless since
-// we are creating an access token for the user after they authorize the application.
-
-type GitConfig struct {
-	UserName       string
-	RepositoryName string
-	Token          string
-	Scm            string // GitHub || GitLab || BitBucket ...
-}
-
-// @TODO Change the name of scm.Issue struct. It conflicts with the struct in issue.go
-type Issue struct {
+type GitIssue struct {
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
 
-// GitConfigManager interface allows us to have different adapters for each
-// source code management system that we would like to use. We can have different
-// implementations for GitHub, GitLab, BitBucket and so on.
-// Authorize creates an access token with scopes that will allow us to read/write issues
-// ReadToken checks if there is an access token in ~/.config/issue-summoner/config.json
+// GitConfigManager provides flexibility to have different implementations
+// of Authorize and Report for each source code management platform supported
 type GitConfigManager interface {
 	Authorize() error
-	Report(issues []Issue) <-chan int64
+	Report(issues []GitIssue) <-chan int64
 }
 
-func NewGitManager(scm string) GitConfigManager {
+// @TODO add other source code management structs to NewGitManager once their implementations are created
+func NewGitManager(scm, userName, repoName string) (GitConfigManager, error) {
 	switch scm {
+	case GITHUB:
+		return &GitHubManager{repoName: repoName, userName: userName}, nil
 	default:
-		return &GitHubManager{}
+		return nil, fmt.Errorf(
+			"expected to receive scm with value of %s, %s, or %s but got %s",
+			GITHUB,
+			GITLAB,
+			BITBUCKET,
+			scm,
+		)
 	}
 }
 
@@ -61,28 +55,29 @@ type IssueSummonerConfig = map[string]ScmTokenConfig
 // This will be used to authorize future requests for reporting issues.
 func WriteToken(token string, scm string) error {
 	config := make(map[string]ScmTokenConfig)
-
-	usr, err := user.Current()
+	path, err := getConfigDirPath()
 	if err != nil {
 		return err
 	}
-
-	home := usr.HomeDir
-	path := filepath.Join(home, ".config", "issue-summoner")
 
 	err = os.MkdirAll(path, 0755)
 	if err != nil {
 		return err
 	}
 
-	configFile := filepath.Join(path, "config.json")
-	file, err := os.OpenFile(configFile, os.O_RDWR|os.O_CREATE, 0666)
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(configFilePath, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		return err
 	}
 
 	defer file.Close()
 
+	// @TODO add remaining source code management platforms once other adapters are implemented
 	switch scm {
 	default:
 		config[GITHUB] = ScmTokenConfig{
@@ -102,53 +97,14 @@ func WriteToken(token string, scm string) error {
 	return nil
 }
 
-// @TODO refactor WriteToken & CheckForAccess functions.
-// There is some DRY code in the two functions that I would like to refactor.
-// Specifically for getting the current directory, home dir and joining the paths
-// for the configuration file.
-func CheckForAccess(scm string) (bool, error) {
-	config := make(map[string]ScmTokenConfig)
-	authorized := false
-
-	usr, err := user.Current()
-	if err != nil {
-		return authorized, err
-	}
-
-	home := usr.HomeDir
-	configFile := filepath.Join(home, ".config", "issue-summoner", "config.json")
-
-	file, err := os.OpenFile(configFile, os.O_RDONLY, 0666)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return authorized, err
-		} else {
-			return authorized, errors.New("Error opening file")
-		}
-	}
-
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(&config); err != nil {
-		return authorized, errors.New("Error decoding config file")
-	}
-
-	return config[scm].AccessToken != "", nil
-}
-
 func ReadAccessToken(scm string) (string, error) {
 	config := make(map[string]ScmTokenConfig)
-
-	usr, err := user.Current()
+	path, err := getConfigFilePath()
 	if err != nil {
 		return "", err
 	}
 
-	home := usr.HomeDir
-	configFile := filepath.Join(home, ".config", "issue-summoner", "config.json")
-
-	file, err := os.OpenFile(configFile, os.O_RDONLY, 0666)
+	file, err := os.OpenFile(path, os.O_RDONLY, 0666)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return "", err
@@ -220,62 +176,20 @@ func extractFromSSH(url []byte) (string, string) {
 	return string(userName), string(bytes.TrimSuffix(repoName, []byte(".git")))
 }
 
-// GlobalUserName uses the **git config** command to retrieve the global
-// configuration options. Specifically, the user.name option. The userName is
-// read and set onto the reciever's (GitConfig) UserName property. This will be used
-func GlobalUserName() (string, error) {
-	var out strings.Builder
-	cmd := exec.Command("git", "config", "--global", "user.name")
-	cmd.Stdout = &out
-
-	err := cmd.Run()
+func getConfigDirPath() (string, error) {
+	usr, err := user.Current()
 	if err != nil {
 		return "", err
 	}
 
-	userName := strings.TrimSpace(out.String())
-	if userName == "" {
-		return "", errors.New("global userName option not set. See man git config for more details")
-	}
-
-	return userName, nil
+	homeDir := usr.HomeDir
+	return filepath.Join(homeDir, ".config", "issue-summoner"), nil
 }
 
-// @TODO write unit test for RepoName/extractRepoName function.
-func RepoName() (string, error) {
-	var out strings.Builder
-	cmd := exec.Command("git", "remote", "-v")
-	cmd.Stdout = &out
-
-	err := cmd.Run()
+func getConfigFilePath() (string, error) {
+	configDir, err := getConfigDirPath()
 	if err != nil {
 		return "", err
 	}
-
-	repoName := extractRepoName(out.String())
-	if repoName == "" {
-		return "", errors.New("Failed to get repo name")
-	}
-
-	return repoName, nil
-}
-
-// extractRepoName takes the output from the `git remote -v` command as input (origins) and outputs the repository name.
-// The function can handle both ssh and https origins.
-// Git does not offer a command that outputs the repository name directly
-func extractRepoName(origins string) string {
-	for _, line := range strings.Split(origins, "\n") {
-		if strings.Contains(line, "(push)") {
-			fields := strings.Fields(line)
-			if len(fields) >= 2 {
-				repoURL := fields[1]
-				parts := strings.Split(repoURL, "/")
-				if len(parts) > 1 {
-					repo := strings.TrimSuffix(parts[len(parts)-1], ".git")
-					return repo
-				}
-			}
-		}
-	}
-	return ""
+	return filepath.Join(configDir, "config.json"), nil
 }
