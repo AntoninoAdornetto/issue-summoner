8801fc558f3891e59dace661ad1a3b78c56f878c
AntoninoAdornetto <armaninollc@protonmail.com>
2024-06-22 18:59:10 -0400

    refactor(v2): pass config to scm managers

--- v2/git/git.go
+++ v2/git/git.go
@@ -1,10 +1,14 @@
 package git
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"time"
+
+	"github.com/AntoninoAdornetto/issue-summoner/pkg/utils"
 )
 
 type sourceCodeManager = string
@@ -15,24 +19,64 @@ const (
 	BITBUCKET sourceCodeManager = "bitbucket"
 )
 
+var (
+	conf = map[string]IssueSummonerConfig{
+		GITHUB:    {},
+		GITLAB:    {},
+		BITBUCKET: {},
+	}
+)
+
 type GitManager interface {
 	Authorize() error
+	IsAuthorized() bool
 }
 
-// @TODO use this once ready for implementation
-// type gitAccessToken struct {
-// 	AccessToken string    `json:"accessToken"`
-// 	CreatedAt   time.Time `json:"createdAt"`
-// 	ExpiresAt   time.Time `json:"expiresAt"`
-// }
+type IssueSummonerConfig struct {
+	Auth authConfig `json:"auth"`
+}
 
+type authConfig struct {
+	AccessToken string    `json:"accessToken"`
+	CreatedAt   time.Time `json:"createdAt"`
+	ExpiresAt   time.Time `json:"expiresAt"`
+}
+
+// NewGitManager will handle specific operations for each source code management platform
+// that the program supports. Each scm should satisfy the GitManager interface for authorizing
+// issue summoner to submit issues to both public/private repositories and support functionality
+// for posting new issues to your code bases. If the issue summoner config file is not created
+// this function will create both the configuration directory and json file. The config file
+// contains details such as access tokens for making http requests to different scm's.
 func NewGitManager(scm sourceCodeManager, repo *Repository) (GitManager, error) {
+	conf := make(map[string]IssueSummonerConfig)
+	data, err := utils.ReadIssueSummonerConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) == 0 {
+		data, err = NewConfig()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err = json.Unmarshal(data, &conf); err != nil {
+		return nil, err
+	}
+
+	// @TODO add support for GitLab and Bitbucket
 	switch scm {
 	case GITHUB:
-		return &githubManager{repo: repo}, nil
+		return &githubManager{config: conf, repo: repo}, nil
 	default:
 		return nil, nil
 	}
+}
+
+func NewConfig() ([]byte, error) {
+	return json.Marshal(conf)
 }
 
 func buildURL(base string, queryParams map[string]string, paths ...string) (string, error) {
