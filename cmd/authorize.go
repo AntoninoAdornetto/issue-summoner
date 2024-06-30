f426276dcc498f92fdb9c2b2b6b6f49781b3b257
AntoninoAdornetto <armaninollc@protonmail.com>
2024-06-30 04:58:14 -0400

    chore: utilize new logger

--- cmd/authorize.go
+++ cmd/authorize.go
@@ -19,37 +19,39 @@ var authorizeCmd = &cobra.Command{
 	Long: `Authorize command is used to create tokens that will allow issue summoner to report issues on your
 	behalf.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		logger := getLogger(cmd)
+
 		srcCodeManager, err := cmd.Flags().GetString("scm")
 		if err != nil {
-			ui.LogFatal(fmt.Errorf("Failed to read 'scm' flag\n%s", err).Error())
+			logger.LogFatal(err.Error())
 		}
 
 		wd, err := os.Getwd()
 		if err != nil {
-			ui.LogFatal(err.Error())
+			logger.LogFatal(err.Error())
 		}
 
 		repo, err := git.NewRepository(wd)
 		if err != nil {
-			ui.LogFatal(err.Error())
+			logger.LogFatal(err.Error())
 		}
 
 		gitManager, err := git.NewGitManager(srcCodeManager, repo)
 		if err != nil {
-			ui.LogFatal(err.Error())
+			logger.LogFatal(err.Error())
 		}
 
 		if gitManager.IsAuthorized() {
-			fmt.Println(
-				ui.PrimaryTextStyle.Render(
-					fmt.Sprintf(
-						"you are authorized for %s for already. Do you want to create a new access token?",
-						srcCodeManager,
-					),
-				),
+			msg := fmt.Sprintf(
+				"you are authorized for %s for already. Do you want to create a new access token?",
+				srcCodeManager,
 			)
+
+			logger.LogWarning(msg)
+
+			// can't use logger for below message, need to scan input from the user
 			fmt.Print(
-				ui.PrimaryTextStyle.Italic(true).
+				ui.NoteTextStyle.Italic(true).
 					Render("Type y to create a new token or type n to cancel the request: "),
 			)
 
@@ -58,23 +60,20 @@ var authorizeCmd = &cobra.Command{
 			proceed := scanner.Text()
 			fmt.Printf("\n\n")
 			if proceed != "y" {
-				ui.LogFatal("Authorization process aborted")
+				logger.LogFatal("Authorization process aborted")
 			}
 		}
 
 		if err := gitManager.Authorize(); err != nil {
-			ui.LogFatal(err.Error())
+			logger.LogFatal(err.Error())
 		}
 
-		fmt.Println(
-			ui.SuccessTextStyle.Render(
-				fmt.Sprintf("Authorization for %s succeeded!", srcCodeManager),
-			),
-		)
+		logger.LogSuccess(fmt.Sprintf("Authorization for %s succeeded!", srcCodeManager))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(authorizeCmd)
 	authorizeCmd.Flags().StringP(flag_scm, shortflag_scm, git.GITHUB, flag_desc_scm)
+	authorizeCmd.Flags().BoolP(flag_debug, shortflag_debug, false, flag_desc_debug)
 }
