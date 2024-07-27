76bd005bdf9efce07adfa8f169d8325da23eb7c0
AntoninoAdornetto <armaninollc@protonmail.com>
2024-07-27 14:20:06 -0600

    docs(v1): responsiblity of the C Lexer

--- pkg/lexer/c.go
+++ pkg/lexer/c.go
@@ -1,3 +1,12 @@
+/*
+Copyright © 2024 AntoninoAdornetto
+
+The c.go file is responsible for satisfying the `LexicalTokenizer` interface in the `lexer.go` file.
+The methods are a strict set of rules for handling single & multi line comments for c-like languages.
+The result, if an issue annotation is located, is a slice of tokens that will provide information about
+the action item contained in the comment. If a comment does not contain an issue annotation, all subsequent
+tokens of the remaining comment bytes will be ignored and removed from the `DraftTokens` slice.
+*/
 package lexer
 
 import (
