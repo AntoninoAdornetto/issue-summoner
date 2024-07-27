4dafc6b36285039bb20357be158746b2f0f5ee8b
AntoninoAdornetto <armaninollc@protonmail.com>
2024-07-27 14:23:45 -0600

    refactor(v1): Lexical Tokenization of SLComments

--- pkg/lexer/c.go
+++ pkg/lexer/c.go
@@ -20,39 +20,104 @@ type Clexer struct {
 	annotated   bool    // indicator to denote when an issue annotation is located
 }
 
+func (c *Clexer) AnalyzeToken() error {
+	currentByte := c.Base.peek()
+	switch currentByte {
+	case QUOTE, DOUBLE_QUOTE, BACK_TICK:
+		return c.String(currentByte)
 	case FORWARD_SLASH:
+		return c.Comment()
 	case NEWLINE:
+		c.Base.Line++
 		return nil
 	default:
 		return nil
 	}
 }
 
+func (c *Clexer) String(delim byte) error {
+	return nil
+}
+
+func (c *Clexer) Comment() error {
+	c.reset()
+	switch c.Base.peekNext() {
 	case FORWARD_SLASH:
+		return c.tokenizeSingleLineComment()
 	default:
 		return nil
 	}
 }
 
+func (c *Clexer) tokenizeSingleLineComment() error {
+	for !c.Base.pastEnd() && c.Base.peek() != NEWLINE {
+		lexeme := c.Base.nextLexeme()
+		if len(lexeme) == 0 {
+			c.Base.next()
+			continue
 		}
 
+		token := NewToken(TOKEN_UNKNOWN, lexeme, c.Base)
+		if err := c.annotateTokenType(&token, TOKEN_SINGLE_LINE_COMMENT); err != nil {
+			return err
 		}
+		c.Base.next()
 	}
 
+	if c.annotated {
+		c.Base.resetStartIndex()
+		c.closeSingleLineCommentToken()
+		c.Base.Tokens = append(c.Base.Tokens, c.DraftTokens...)
 	}
 
 	return nil
 }
 
+func (c *Clexer) annotateTokenType(token *Token, tokenType TokenType) error {
+	isSingle := containsBits(tokenType, TOKEN_SINGLE_LINE_COMMENT)
+	isMulti := !isSingle && containsBits(tokenType, TOKEN_MULTI_LINE_COMMENT)
+
+	switch {
+	case isSingle:
+		c.annotateSingleLineComment(token)
+	case isMulti:
+	default:
+		return c.reportTokenTypeError(tokenType)
 	}
+
+	c.DraftTokens = append(c.DraftTokens, *token)
 	return nil
 }
 
+var (
+	cSingleLineCommentNotation = []byte("//")
+)
+
+func (c *Clexer) annotateSingleLineComment(token *Token) {
+	switch {
+	case !c.annotated && bytes.Equal(c.Base.Annotation, token.Lexeme):
+		token.Type = TOKEN_COMMENT_ANNOTATION
+		c.annotated = true
+	case bytes.Equal(cSingleLineCommentNotation, token.Lexeme):
+		token.Type = TOKEN_SINGLE_LINE_COMMENT_START
+	default:
+		token.Type = TOKEN_COMMENT_TITLE
 	}
 }
 
+func (c *Clexer) closeSingleLineCommentToken() {
+	token := NewToken(TOKEN_SINGLE_LINE_COMMENT_END, make([]byte, 0, 1), c.Base)
 
+	if c.Base.peek() == NEWLINE {
+		c.Base.Line++
+		token.Lexeme = append(token.Lexeme, byte('\n'))
+	} else {
+		token.Lexeme = append(token.Lexeme, 0)
 	}
+
+	c.DraftTokens = append(c.DraftTokens, token)
+}
+
 }
 
 }
