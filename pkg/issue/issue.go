80fbd30c88a67398b5255e78cc49f352d9fde36c
AntoninoAdornetto <armaninollc@protonmail.com>
2024-08-17 17:05:58 -0600

    chore(v1): instantiate issue manager

--- pkg/issue/issue.go
+++ pkg/issue/issue.go
@@ -1,10 +1,8 @@
 package issue
 
 import (
-	"bytes"
 	"errors"
 	"runtime"
-	"text/template"
 )
 
 type IssueMode = string
@@ -14,39 +12,22 @@ const (
 	IssueModeScan  IssueMode = "scan"
 )
 
-type Issue struct {
-	ID          string
-	Title       string
-	Description string
-	FilePath    string
-	FileName    string
-	LineNumber  int
-	Environment string
-	StartIndex  int
-	EndIndex    int
+type IssueManager struct {
+	Issues     []Issue
+	annotation []byte
+	mode       IssueMode
+	os         string
 }
 
-type IssueManager interface {
-	GetIssues() []Issue
-	Scan(src []byte, path string) error
-	Walk(root string) (int, error)
-	WriteIssueID(id int64, issueIndex int) error
-}
+type Issue struct{}
 
-// NewIssueManager will return either a PendingIssue struct or ProcessedIssue struct
-// that satisfies the methods defined in the IssueManager interface. The methods in
-// said interface are used to report new issues an SCM or locate issues that have been
-// reported to an SCM. Each struct will implement methods for walking the project directory
-// and parsing source code files. The main difference is that pending issues will have an
-// annotation with no id, since they haven't been pushed to an scm yet, and processed issues
-// will have their original annotation plus an id so they can be located and removed from the
-// source code file at a later time.
-func NewIssueManager(issueType string, annotation string) (IssueManager, error) {
-	switch issueType {
-	case PENDING_ISSUE:
-		return &PendingIssue{Annotation: annotation}, nil
-	case PROCESSED_ISSUE:
-		return &ProcessedIssue{Annotation: annotation}, nil
+func NewIssueManager(annotation []byte, mode IssueMode) (*IssueManager, error) {
+	manager := &IssueManager{
+		Issues: make([]Issue, 0),
+		mode:   mode,
+		os:     runtime.GOOS,
+	}
+
 	switch mode {
 	case IssueModeScan:
 		manager.annotation = annotation
@@ -56,11 +37,6 @@ func NewIssueManager(issueType string, annotation string) (IssueManager, error) 
 	default:
 		return nil, errors.New("expected mode of \"report\" or \"purge\"")
 	}
-}
 
-func (issue *Issue) ExecuteIssueTemplate(tmpl *template.Template) ([]byte, error) {
-	buf := bytes.Buffer{}
-	issue.Environment = runtime.GOOS
-	err := tmpl.Execute(&buf, issue)
-	return buf.Bytes(), err
+	return manager, nil
 }
