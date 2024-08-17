94dc2699f56ac3c5d4a3dafd2719e0beaf36b3fe
AntoninoAdornetto <armaninollc@protonmail.com>
2024-08-16 23:52:45 -0600

    refactor(v1): split value into title & desc

--- pkg/lexer/comment.go
+++ pkg/lexer/comment.go
@@ -1,25 +1,57 @@
 package lexer
 
+import (
+	"bytes"
+)
+
 type Comment struct {
-	Title          []byte
-	Description    []byte
-	TokenIndex     int
-	Source         []byte
-	SourceFileName string
+	Title, Description   string
+	TokenStartIndex      int
+	TokenAnnotationIndex int
+	TokenEndIndex        int
+	LineNumber           int
 }
 
-func (c *Comment) Prepare(fileName string, index int) {
-	c.TokenIndex = index
-	c.SourceFileName = fileName
+type CommentManager struct {
+	Comments []Comment
 }
 
-func (c *Comment) Validate() bool {
-	return len(c.Source) > 0
+func BuildComments(tokens []Token) CommentManager {
+	manager := CommentManager{Comments: make([]Comment, 0, 10)}
+
+	for i := 0; i < len(tokens); i++ {
+		cur := tokens[i]
+		if cur.Type == TOKEN_SINGLE_LINE_COMMENT_START ||
+			cur.Type == TOKEN_MULTI_LINE_COMMENT_START {
+			manager.iterCommentEnd(tokens, &i)
+		}
+	}
+
+	return manager
 }
 
-func (c *Comment) Push(comments *[]Comment, fileName string, tokenIndex int) {
-	if c.Validate() {
-		c.Prepare(fileName, tokenIndex)
-		*comments = append(*comments, *c)
+func (m *CommentManager) iterCommentEnd(tokens []Token, index *int) {
+	token := tokens[*index]
+	comment := Comment{LineNumber: token.Line}
+	title, description := make([][]byte, 0), make([][]byte, 0)
+
+	for ; *index < len(tokens); *index++ {
+		token = tokens[*index]
+		switch token.Type {
+		case TOKEN_SINGLE_LINE_COMMENT_START, TOKEN_MULTI_LINE_COMMENT_START:
+			comment.TokenStartIndex = *index
+		case TOKEN_COMMENT_ANNOTATION:
+			comment.TokenAnnotationIndex = *index
+		case TOKEN_COMMENT_TITLE:
+			title = append(title, token.Lexeme)
+		case TOKEN_COMMENT_DESCRIPTION:
+			description = append(description, token.Lexeme)
+		case TOKEN_SINGLE_LINE_COMMENT_END, TOKEN_MULTI_LINE_COMMENT_END:
+			comment.TokenEndIndex = *index
+			comment.Title = string(bytes.Join(title, []byte(" ")))
+			comment.Description = string(bytes.Join(description, []byte(" ")))
+			m.Comments = append(m.Comments, comment)
+			return
+		}
 	}
 }
