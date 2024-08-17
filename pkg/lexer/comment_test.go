84f8dc2cd4de40d32379b6609cc7ec469478a67b
AntoninoAdornetto <armaninollc@protonmail.com>
2024-08-16 23:53:27 -0600

    test(comment): with line numbers

--- pkg/lexer/comment_test.go
+++ pkg/lexer/comment_test.go
@@ -18,28 +18,37 @@ func TestBuildComments(t *testing.T) {
 			path: "../../testdata/fixtures/c/mix.c",
 			expected: []lexer.Comment{
 				{
-					Value:                "inline comment #1",
+					Title:                "inline comment #1",
+					Description:          "", // single line comments don't have descriptions
 					TokenStartIndex:      0,
 					TokenAnnotationIndex: 1,
 					TokenEndIndex:        5,
+					LineNumber:           5,
 				},
 				{
-					Value:                "inline comment #2",
+					Title:                "inline comment #2",
+					Description:          "", // single line comments don't have descriptions
 					TokenStartIndex:      6,
 					TokenAnnotationIndex: 7,
 					TokenEndIndex:        11,
+					LineNumber:           6,
 				},
 				{
-					Value:                "decode the message and clean up after yourself!",
+					Title:                "decode the message and clean up after yourself!",
+					Description:          "",
 					TokenStartIndex:      12,
 					TokenAnnotationIndex: 13,
 					TokenEndIndex:        22,
+					LineNumber:           10,
 				},
 				{
-					Value:                "drop a star if you know about this code wars challenge Digital Cypher assigns to each letter of the alphabet unique number. Instead of letters in encrypted word we write the corresponding number Then we add to each obtained digit consecutive digits from the key",
+					// multi line comments have a description
+					Title:                "drop a star if you know about this code wars challenge",
+					Description:          "Digital Cypher assigns to each letter of the alphabet unique number. Instead of letters in encrypted word we write the corresponding number Then we add to each obtained digit consecutive digits from the key",
 					TokenStartIndex:      23,
 					TokenAnnotationIndex: 24,
 					TokenEndIndex:        70,
+					LineNumber:           14,
 				},
 			},
 		},
