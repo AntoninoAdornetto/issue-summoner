9a03a60023f694442340c13812cabe6ad4cfae90
AntoninoAdornetto <armaninollc@protonmail.com>
2024-09-02 21:31:34 -0600

    fix: incorrect start/end indices

--- pkg/lexer/comment.go
+++ pkg/lexer/comment.go
@@ -40,7 +40,7 @@ func (m *CommentManager) iterCommentEnd(tokens []Token, index *int) {
 		token = tokens[*index]
 		switch token.Type {
 		case TOKEN_SINGLE_LINE_COMMENT_START, TOKEN_MULTI_LINE_COMMENT_START:
-			comment.TokenStartIndex = *index
+			comment.TokenStartIndex = token.Start
 		case TOKEN_COMMENT_ANNOTATION:
 			comment.TokenAnnotationIndex = *index
 			comment.AnnotationPos = []int{token.Start, token.End}
@@ -49,7 +49,7 @@ func (m *CommentManager) iterCommentEnd(tokens []Token, index *int) {
 		case TOKEN_COMMENT_DESCRIPTION:
 			description = append(description, token.Lexeme)
 		case TOKEN_SINGLE_LINE_COMMENT_END, TOKEN_MULTI_LINE_COMMENT_END:
-			comment.TokenEndIndex = *index
+			comment.TokenEndIndex = token.End
 			comment.Title = string(bytes.Join(title, []byte(" ")))
 			comment.Description = string(bytes.Join(description, []byte(" ")))
 			m.Comments = append(m.Comments, comment)
