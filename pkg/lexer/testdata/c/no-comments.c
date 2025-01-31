d5c69a266e965de400cad196f2e413eeda8b3fd3
AntoninoAdornetto <armaninollc@protonmail.com>
2025-01-31 14:19:44 -0700

    chore: rust rewrite

--- pkg/lexer/testdata/c/no-comments.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-  printf("No comments in this src file\n");
-  return 0;
-}
--- pkg/lexer/testdata/js/mix.js
+++
@@ -1,5 +0,0 @@
-function normalize(str) { // @TEST_ANNOTATION fix bug in v8
-  return str.split("\u0000");
-}
-
-/***/
